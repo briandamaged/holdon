104b260954df4fb493273ff8187a8fb6271dde8e
Brian Lauber <blauber@jibe.com>
2013-10-29 23:26:22 -0400

    Initial commit

---
+++ holdon.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'holdon'
+  s.version     = '0.0.1'
+  s.date        = '2013-10-29'
+  s.summary     = "HoldOn until a condition is true"
+  s.description = "HoldOn until a condition is true"
+  s.authors     = ["Brian Lauber"]
+  s.email       = 'constructible.truth@gmail.com'
+  s.files       = Dir["lib/**/*.rb"]
+  s.license     = "MIT"
+end
---
+++ lib/holdon.rb
@@ -0,0 +1,29 @@
+
+module HoldOn
+
+  class Timeout < Exception
+  end
+
+
+  def self.until(options = {})
+    timeout  = options.fetch(:timeout, 30)
+    interval = options.fetch(:interval, 1)
+
+    start = Time.now
+    loop do
+      break if yield
+
+      # If the time remaining is less than the interval,
+      # then we'll only sleep for the time remaining.
+      inv = [interval, (timeout - (Time.now - start))].min
+      if inv > 0
+        sleep inv
+      else
+        raise Timeout
+      end
+
+    end
+  end
+
+end
+
